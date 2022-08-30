d27f4cac7969d252b4af2fa516b76a030be33358
Redouane Achouri <redouane.a.achouri@gmail.com>
2022-08-30 22:47:44 +0200

    Cleanup output and add kubernetes_load_balancer_dns_name

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,11 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.vpc.id
-}
-
-output "subnet_id" {
-  description = "ID of the subnet"
-  value       = aws_subnet.subnet.id
-}
-
-output "security_group_allow_internal_id" {
-  description = "ID of the internal traffic security group"
-  value       = aws_security_group.security_group_allow_internal.id
-}
-
-output "security_group_allow_external_id" {
-  description = "ID of the external traffic security group"
-  value       = aws_security_group.security_group_allow_external.id
-}
-
-output "access_key_name" {
-  description = "EC2 access key name"
-  value       = aws_key_pair.access_key.key_name
-}
-
 output "kubernetes_public_ip_address" {
   description = "Public EIP address of the Kubernetes cluster"
   value       = aws_eip.kubernetes_public_ip.public_ip
+}
+
+output "kubernetes_load_balancer_dns_name" {
+  description = "Public DNS name of the Kubernetes cluster"
+  value       = aws_lb.controllers_load_balancer.dns_name
 }
 
 output "kubernetes_controllers_private_ip_addresses" {
